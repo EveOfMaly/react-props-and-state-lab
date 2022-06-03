9e38671c620e018b8f045336a1d9beb9f106a699
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-06-02 21:00:39 -0700

    Completed Pet Rendering Props

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,8 +4,27 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return this.props.pets.map((data, idx) => {
+      return (
+        <div key={idx} className="ui cards"><Pet pet={data}/></div>
+      )
+    })
+    
+    
   }
 }
 
 export default PetBrowser
+
+// class PetBrowser extends React.Component {
+//   render() {
+//     return (<div> {this.props.pets.map((pet,idx) => 
+//     {
+//       return (
+//       <div className="ui cards"><Pet pet={pet}/></div>)}</div>)
+//       )
+//     }
+//     // <div className="ui cards"><Pet /></div>
+//   }
+// }
+
